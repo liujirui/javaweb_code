4e8884eca61e0b6345051222653212b3b9b39741
liu <1301100836@qq.com>
2017-08-16 23:54:27 +0800

    工具类、静态代码块优化性能

--- hibernate_day01/src/cn/liu/hibernatetest/HibernateDemo.java
+++ hibernate_day01/src/cn/liu/hibernatetest/HibernateDemo.java
@@ -10,6 +10,7 @@ import org.hibernate.cfg.Configuration;
 import org.junit.Test;
 
 import cn.liu.hibernate.User;
+import cn.liu.hibernateutils.HibernateUtils;
 
 
 public class HibernateDemo 
@@ -19,11 +20,13 @@ public class HibernateDemo
 	public void addtest()
 	{
 //		加载核心配置文件
-		Configuration configuration=new Configuration();
-		configuration.configure();
+//		Configuration configuration=new Configuration();
+//		configuration.configure();
 //		创建SessionFactory对象
 		/*根据映射关系，在数据库创建表*/
-		SessionFactory sessionFactory=configuration.buildSessionFactory();
+//		SessionFactory sessionFactory=configuration.buildSessionFactory();
+		/*使用工具类获取sessionfactory,优化性能*/
+		SessionFactory sessionFactory=HibernateUtils.getSessionFactory();
 //		创建session
 		/*类似于连接*/
 		Session session=sessionFactory.openSession();
@@ -31,9 +34,9 @@ public class HibernateDemo
 		Transaction transaction=session.beginTransaction();
 //		写crud具体逻辑
 		User user=new User();
-		user.setUsername("zhu");
-		user.setPassword("13");
-		user.setAddress("shanghai");
+		user.setUsername("wang");
+		user.setPassword("17");
+		user.setAddress("nanning");
 		session.save(user);
 //		提交事务
 		transaction.commit();
